96ebd2035a4ec16c589367c3141725baa6f3f543
Marc Neudert <marc.neudert@gmail.com>
2017-05-05 15:51:43 +0200

    provides script for mmdb2 generation

---
+++ mmdb2/write-test-data.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use MaxMind::DB::Writer::Tree;
+use Net::Works::Network;
+
+my $tree = MaxMind::DB::Writer::Tree->new(
+    ip_version            => 6,
+    record_size           => 24,
+    alias_ipv6_to_ipv4    => 1,
+    database_type         => 'Geolix-Test',
+    languages             => ['en'],
+    description           => { en => 'Geolix test database' },
+    map_key_type_callback => sub { 'utf8_string' },
+);
+
+my $network = Net::Works::Network->new_from_string(
+    string => '1.1.1.1/32'
+);
+
+$tree->insert_network( $network, { type => 'test' } );
+
+open my $fh, '>', './Geolix.mmdb';
+$tree->write_tree($fh);
+close $fh;
