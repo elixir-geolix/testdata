de851953ee6450adafde3f0c720746f207661612
Marc Neudert <marc.neudert@gmail.com>
2018-07-10 19:31:38 +0200

    bumps version to v0.4.0-dev

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Geolix.TestData.Mixfile do
     [
       app: :geolix_testdata,
       name: "Geolix Test Data Provider",
-      version: "0.3.0",
+      version: "0.4.0-dev",
       elixir: "~> 1.3",
       deps: [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}],
       description: "Geolix Test Data Provider",
@@ -19,7 +19,7 @@ defmodule Geolix.TestData.Mixfile do
   defp docs do
     [
       main: "Geolix.TestData",
-      source_ref: "v0.3.0",
+      source_ref: "master",
       source_url: @url_github
     ]
   end
