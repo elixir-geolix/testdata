287aad62847d4f5121ea650110d049c9f9f1e119
Marc Neudert <marc.neudert@gmail.com>
2022-06-22 17:04:31 +0200

    Use attribute for version

--- mix.exs
+++ mix.exs
@@ -2,12 +2,13 @@ defmodule Geolix.TestData.MixProject do
   use Mix.Project
 
   @url_github "https://github.com/elixir-geolix/testdata"
+  @version "0.6.0-dev"
 
   def project do
     [
       app: :geolix_testdata,
       name: "Geolix Test Data Provider",
-      version: "0.6.0-dev",
+      version: @version,
       elixir: "~> 1.7",
       deps: [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}],
       description: "Geolix Test Data Provider",
@@ -25,7 +26,7 @@ defmodule Geolix.TestData.MixProject do
   defp docs do
     [
       main: "Geolix.TestData",
-      source_ref: "master",
+      source_ref: "v#{@version}",
       source_url: @url_github
     ]
   end
