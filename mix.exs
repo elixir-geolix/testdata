548a3d8e573aa66bfb1b39c0860b8acc86b28398
Marc Neudert <marc.neudert@gmail.com>
2022-08-12 15:53:51 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule Geolix.TestData.MixProject do
   defp deps do
     [
       {:castore, "~> 0.1.0"},
-      {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
